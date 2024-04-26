2b0cb98d7d12ccbaaa42f15b21e83cf697d8b650
jojo <jyotidkarki@gmail.com>
2024-04-26 21:50:06 +0530

    firslmjkm

--- 3_subnets.tf
+++ 3_subnets.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "prod-subnet-public-1" {
   vpc_id                  = aws_vpc.nginx-vpc.id // Referencing the id of the VPC from abouve code block
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true" // Makes this a public subnet
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1a"
   tags = {
     name = "public-subnet"
   }
@@ -13,7 +13,7 @@ resource "aws_subnet" "prod-subnet-public-1" {
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                  = aws_vpc.nginx-vpc.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
     Name = "private-subnet-1"
